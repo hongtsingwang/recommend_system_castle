63038d7f0189b30522b3138e9ee27733a53fec0f
王洪庆 <wanghongqing@sensorsdata.cn>
2021-12-03 13:36:58 +0800

    增加DEBUG模式

--- data/run_data.py
+++ data/run_data.py
@@ -7,6 +7,8 @@ from tqdm import tqdm
 
 from process_data import process_data
 
+# 是否为DEBUG模式
+debug_mode = False
 # 路径定义
 current_dir = os.getcwd()
 # 代码根目录
@@ -16,7 +18,13 @@ input_dir = os.path.join(base_dir, "input")
 # 基于输入数据， 生成的训练样本的目录
 sample_dir = os.path.join(base_dir, "sample")
 # user-item_id打分文件路径
-ratings_file_path = os.path.join(input_dir, "ml-20m", "ratings.csv")
+if not debug_mode:
+    ratings_file_path = os.path.join(input_dir, "ml-20m", "ratings.csv")
+else:
+    ratings_file_path = os.path.join(input_dir, "ml-20m", "ratings_test.csv")
+
+print("ratings_file_path is:", ratings_file_path)
+
 
 train_file_path = os.path.join(sample_dir, "train.csv")
 test_file_path = os.path.join(sample_dir, "test.csv")
@@ -30,13 +38,13 @@ data_info_file_path = os.path.join(sample_dir, "data_info.pkl")
 
 # 参数配置, 当前所有参数都是随便拍的
 # 负样本的比例
-negative_sample_ratio = 3.0
+negative_sample_ratio = 1.0
 # 负样本采样阈值， 在这个分数之上的都是正样本， 在这个阈值之下的是非正样本
 negative_sample_threshold = 0.5
 # 采样方式， 计算item出现频率， 出现频率越高， 约容易被抽样为负样本
 negative_sample_method = "popular"
 # 测试集比例
-split_test_ratio = 0.2
+split_test_ratio = 0.4
 # 是否打乱重排
 shuffle_before_split = True
 split_ensure_positive = True
@@ -59,12 +67,14 @@ with open(data_info_file_path, "wb") as f:
 
 with open(train_file_path, "w") as f:
     for data in tqdm(train_data):
+        data = [str(i) for i in data]
         user_id, item_id, label = data
         f.write("\t".join([user_id, item_id, label]))
         f.write("\n")
 
 with open(test_file_path, "w") as f:
     for data in tqdm(test_data):
+        data = [str(i) for i in data]
         user_id, item_id, label = data
         f.write("\t".join([user_id, item_id, label]))
         f.write("\n")
