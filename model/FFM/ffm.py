e69c1951da866feff839f8449adeedb9a34ab7a9
王洪庆 <wanghongqing@sensorsdata.cn>
2021-12-07 00:34:58 +0800

    增加FM模型以及FFM模型

--- model/FFM/ffm.py
+++ model/FFM/ffm.py
@@ -1,37 +1,44 @@
 # coding=utf-8
 
 import sys
+import tensorflow as tf
 import tensorflow.kears.backend as K
 from tensorflow.keras import Model
 from tensorflow.keras.regularizers import l1, l2
 from keras.layers import Activation
-from tensorflow.keras.layers import Input, Dense, Layer, Add
+from tensorflow.keras.layers import Input, Dense, Layer, Add, Reshape
 
 sys.path.append("..")
 from base_model import BaseModel
 
 class CrossLayer(Layer):
-    def __init__(self, input_dim, output_dim=30, **kwargs):
+    def __init__(self, field_dict, field_dim, input_dim, output_dim=30,  **kwargs):
+        self.field_dict = field_dict
+        self.field_dim = field_dim
         self.input_dim = input_dim
         self.output_dim = output_dim
         super(CrossLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
         self.kernel = self.add_weight(name='kernel', 
-                                      shape=(self.input_dim, self.output_dim),
+                                      shape=(self.input_dim, self.field_dim, self.output_dim),
                                       initializer='glorot_uniform',
                                       trainable=True)
         super(CrossLayer, self).build(input_shape)
 
     def call(self, x):
-        a = K.pow(K.dot(x,self.kernel), 2)
-        b = K.dot(K.pow(x, 2), K.pow(self.kernel, 2))
-        return K.mean(a-b, 1, keepdims=True)*0.5
+        self.field_cross = K.variable(0, dtype='float32')
+        for i in range(self.input_dim):
+            for j in range(i+1, self.input_dim):
+                weight = tf.math.reduce_sum(tf.math.multiply(self.kernel[i, self.field_dict[j]], self.kernel[j, self.field_dict[i]]))
+                value = tf.math.multiply(weight, tf.math.multiply(x[:,i], x[:,j]))
+                self.field_cross = tf.math.add(self.field_cross, value)
+        return self.field_cross
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], self.output_dim)
 
-class FmModel(BaseModel):
+class FfmModel(BaseModel):
     def __init__(
         self,
         units_num=1,
@@ -41,15 +48,21 @@ class FmModel(BaseModel):
         optimizer="Adam",
         loss="binary_crossentropy",
         metrics="binary_accuracy",
-        bias_regularizer=0.01,
-        kernel_regularizer=0.02,
+        # bias_regularizer=0.01,
+        # kernel_regularizer=0.02,
         model_name="lr",
-        tb_log_path="./lr_model"
+        tb_log_path="./lr_model",
+        field_dim=None,
+        output_dim=30,
+        field_dict = None
     ) -> None:
         self.units_num = units_num
-        self.bias_regularizer = bias_regularizer
-        self.kernel_regularizer = kernel_regularizer
-        super(FmModel, self).__init__(
+        self.field_dim = field_dim
+        self.output_dim = output_dim
+        self.field_dict = field_dict
+        # self.bias_regularizer = bias_regularizer
+        # self.kernel_regularizer = kernel_regularizer
+        super(FfmModel, self).__init__(
             lr=lr, epochs=epochs, optimizer=optimizer, loss=loss, metrics=metrics, batch_size=batch_size,
             tb_log_path=tb_log_path, model_name=model_name
         )
@@ -61,10 +74,11 @@ class FmModel(BaseModel):
         linear_part = Dense(
             1,
             name="linear",
-            bias_regularizer=l2(self.bias_regularizer),
-            kernel_regularizer=l1(self.kernel_regularizer)
+            # bias_regularizer=l2(self.bias_regularizer),
+            # kernel_regularizer=l1(self.kernel_regularizer)
         )(inputs)
-        cross_part = CrossLayer(self.units_num)(inputs)
+        cross_part = CrossLayer(self.field_dict, self.field_dim, self.units_num, self.output_dim)(inputs)
+        cross_part = Reshape((1,))(cross_part)
         add_layer = Add()([linear_part, cross_part])
         outputs = Activation("sigmoid")(add_layer)
         model = Model(inputs, outputs)
--- model/FFM/run_ffm.py
+++ model/FFM/run_ffm.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from fm import FmModel
 
 # 载入数据
+field_dict = {i:i//5 for i in range(30)}
 breast_cancer_data = load_breast_cancer()
 # 模型初始化，配置参数
 units_num = len(breast_cancer_data.feature_names)
@@ -13,7 +14,7 @@ epochs = 10
 batch_size = 8
 lr = 0.001
 output_path = "./fm_model.h5"
-lr_model = FmModel(units_num=units_num, epochs=epochs, batch_size=batch_size, lr=lr)
+ffm_model = FmModel(units_num=units_num, epochs=epochs, batch_size=batch_size, lr=lr)
 X_train, X_test, y_train, y_test = train_test_split(breast_cancer_data.data, breast_cancer_data.target, test_size=0.2,
                                                     random_state=42, stratify=breast_cancer_data.target)
 lr_model.compile()
